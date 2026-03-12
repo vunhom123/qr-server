ecf721a35b64ea3aefdf8653fbc7d5cb4472eb1e
Vunhom123 <nnv242@gmail.com>
2026-03-12 15:51:47 +0700

    fix route

--- server.js
+++ server.js
@@ -9,25 +9,21 @@ const io = new Server(server, {
   cors: { origin: "*" },
 });
 
-io.on("connection", () => {
-  console.log("dashboard connected");
+app.get("/", (req, res) => {
+  res.send("QR server running");
 });
 
 app.get("/scan", (req, res) => {
   const qr = req.query.qr;
 
-  const data = {
+  io.emit("new_scan", {
     code: qr,
     time: new Date().toLocaleTimeString(),
-  };
-
-  io.emit("new_scan", data);
+  });
 
   res.send("scan ok");
 });
 
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => {
+server.listen(process.env.PORT || 5000, () => {
   console.log("server running");
 });
